bda94f1b12fc13bfe72e4245769bdc6b7ffe9f81
kevinshieldsjr <amstineproject@gmail.com>
2022-05-03 14:56:02 -0400

    Updated States

--- src/stateTaxes.java
+++ src/stateTaxes.java
@@ -2,60 +2,60 @@ import java.util.*;
 
 public class stateTaxes{
 
-    public static void main(String[] args) {
+
         // write your code here
         final double Alabama = 0.04; //Alabama
-        final double two = 0.00; //Alaska
-        final double three = 0.056; //Arizona
-        final double four = 0.065; //Arkansas
-        final double five = 0.0725; //California
-        final double six = 0.029; //Colorado
-        final double seven = 0.0635; //Connecticut
-        final double eight = 0.00; //Delaware
-        final double nine = 0.06; //District of Columbia
-        final double ten = 0.06; //Florida
-        final double eleven = 0.04; //Georgia
-        final double twelve = 0.04; //Hawaii
-        final double thirteen = 0.06; //Idaho
-        final double fourteen = 0.0625; //Illinois
-        final double fifteen = 0.07; //Indiana
-        final double sixteen = 0.06; //Iowa
-        final double seventeen = 0.065; //Kansas
-        final double eighteen = 0.06; //Kentucky
-        final double nineteen = 0.0445; //Louisiana
-        final double twenty = 0.055; //Maine
-        final double twentyone = 0.06; //Maryland
-        final double twentytwo = 0.0625; //Massachusetts
-        final double twentythree = 0.06; //Michigan
-        final double twentyfour = 0.0688; //Minnesota
-        final double twentyfive = 0.07; //Mississippi
-        final double twentysix = 0.0423; //Missouri
-        final double twentyseven = 0.00; //Montana
-        final double twentyeight = 0.055; //Nebraska
-        final double twentynine = 0.0685; //Nevada
-        final double thirty = 0.00; //New Hampshire
-        final double thirtyone = 0.0663; //New Jersey
-        final double thirtytwo = 0.0513; //New Mexico
-        final double thirtythree = 0.04; //New York
-        final double thirtyfour = 0.0475; //North Carolina
-        final double thirtyfive = 0.05; //North Dakota
-        final double thirtysix = 0.0575; //Ohio
-        final double thirtyseven = 0.045; //Oklahoma
-        final double thirtyeight = 0.00; //Oregon
-        final double thirtynine = 0.06; //Pennsylvania
-        final double fourty = 0.07; //Rhode Island
-        final double fourtyone = 0.06; //South Carolina
-        final double fourtytwo = 0.045; // South Dakota
-        final double fourtythree = 0.07; //Tennessee
-        final double fourtyfour = 0.0625; //Texas
-        final double fourtyfive = 0.061; //Utah
-        final double fourtysix = 0.06; //Vermont
-        final double fourtyseven = 0.053; //Virginia
-        final double fourtyeight = 0.065; //Washington
-        final double fourtynine = 0.06; //West Virginia
-        final double fifty = 0.05; //Wisconsin
-        final double fiftyone = 0.04; //Wyoming
+        final double Alaska = 0.00; //Alaska
+        final double Arizona = 0.056; //Arizona
+        final double Arkansas = 0.065; //Arkansas
+        final double California = 0.0725; //California
+        final double Colorado = 0.029; //Colorado
+        final double Connecticut = 0.0635; //Connecticut
+        final double Delaware = 0.00; //Delaware
+        final double Dc = 0.06; //District of Columbia
+        final double Florida = 0.06; //Florida
+        final double Georgia = 0.04; //Georgia
+        final double Hawaii = 0.04; //Hawaii
+        final double Idaho = 0.06; //Idaho
+        final double Illinois = 0.0625; //Illinois
+        final double Indiana = 0.07; //Indiana
+        final double Iowa = 0.06; //Iowa
+        final double Kansas = 0.065; //Kansas
+        final double Kentucky = 0.06; //Kentucky
+        final double Louisiana = 0.0445; //Louisiana
+        final double Maine = 0.055; //Maine
+        final double Maryland = 0.06; //Maryland
+        final double Massachusetts = 0.0625; //Massachusetts
+        final double Michigan = 0.06; //Michigan
+        final double Minnesota = 0.0688; //Minnesota
+        final double Mississippi = 0.07; //Mississippi
+        final double Missouri = 0.0423; //Missouri
+        final double Montana = 0.00; //Montana
+        final double Nebraska = 0.055; //Nebraska
+        final double Nevada = 0.0685; //Nevada
+        final double Newhampshire = 0.00; //New Hampshire
+        final double Newjersey = 0.0663; //New Jersey
+        final double Newmexico = 0.0513; //New Mexico
+        final double Newyork = 0.04; //New York
+        final double Northcarolina = 0.0475; //North Carolina
+        final double Northdakota = 0.05; //North Dakota
+        final double Ohio = 0.0575; //Ohio
+        final double Oklahoma = 0.045; //Oklahoma
+        final double Oregon = 0.00; //Oregon
+        final double Pennsylvania = 0.06; //Pennsylvania
+        final double Rhodeisland = 0.07; //Rhode Island
+        final double Southcarolina = 0.06; //South Carolina
+        final double Southdakota = 0.045; // South Dakota
+        final double Tennessee = 0.07; //Tennessee
+        final double Texas = 0.0625; //Texas
+        final double Utah = 0.061; //Utah
+        final double Vermont = 0.06; //Vermont
+        final double Virginia = 0.053; //Virginia
+        final double Washington = 0.065; //Washington
+        final double Westvirginia = 0.06; //West Virginia
+        final double Wisconsin = 0.05; //Wisconsin
+        final double Wyoming = 0.04; //Wyoming
 
     }
-}
+
 
