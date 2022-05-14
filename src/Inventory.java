4157168a246ed69c148c169441b1bc4f36ec4a04
KCA147Project <103001624+KCA147Project@users.noreply.github.com>
2022-05-14 14:59:19 -0400

    Created new class for inventory. added calling statement for new class (AS)

---
+++ src/Inventory.java
@@ -0,0 +1,43 @@
+public class Inventory {
+
+
+    public static int[] itemInventory() {
+
+
+        int[] itemInventory = new int[30];
+
+        itemInventory[0] = 15;
+        itemInventory[1] = 30;
+        itemInventory[2] = 7;
+        itemInventory[3] = 3;
+        itemInventory[4] = 9;
+        itemInventory[5] = 13;
+        itemInventory[6] = 12;
+        itemInventory[7] = 9;
+        itemInventory[8] = 18;
+        itemInventory[9] = 22;
+        itemInventory[10] = 21;
+        itemInventory[11] = 25;
+        itemInventory[12] = 26;
+        itemInventory[13] = 13;
+        itemInventory[14] = 9;
+        itemInventory[15] = 29;
+        itemInventory[16] = 14;
+        itemInventory[17] = 26;
+        itemInventory[18] = 15;
+        itemInventory[19] = 11;
+        itemInventory[20] = 18;
+        itemInventory[21] = 9;
+        itemInventory[22] = 24;
+        itemInventory[23] = 23;
+        itemInventory[24] = 14;
+        itemInventory[25] = 26;
+        itemInventory[26] = 14;
+        itemInventory[27] = 13;
+        itemInventory[28] = 5;
+        itemInventory[29] = 6;
+
+        return itemInventory;
+    }
+
+}
--- src/stateTaxes.java
+++ src/stateTaxes.java
@@ -1,64 +1,66 @@
 import java.util.*;
 public class stateTaxes {
-    public stateTaxes() {
+    public double stateTaxes() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a State Abreviation (e.g. CA): " );
         String stateName = input.nextLine();
+        double tax = 0.00;
         switch (stateName) {
-            case "AL" -> {final double tax = 0.04; }//Alabama
-            case "AK" -> {final double tax = 0.00;} //Alaska
-            case "AZ" -> {final double AZ = 0.056;} //Arizona
-            case "AR" -> {final double AR = 0.065;} //Arkansas
-            case "CA" -> {final double CA = 0.0725;} //California
-            case "CO" -> {final double CO = 0.029;} //Colorado
-            case "CT" -> {final double CT = 0.0635;} //Connecticut
-            case "DE" -> {final double DE = 0.00;} //Delaware
-            case "DC" -> {final double DC = 0.06;} //District of Columbia
-            case "FL" -> {final double FL = 0.06;} //Florida
-            case "GA" -> {final double GA = 0.04;} //Georgia
-            case "HI" -> {final double HI = 0.04;} //Hawaii
-            case "ID" -> {final double ID = 0.06;} //Idaho
-            case "IL" -> {final double IL = 0.0625;} //Illinois
-            case "IN" -> {final double IN = 0.07;} //Indiana
-            case "IA" -> {final double IA = 0.06;} //Iowa
-            case "KS" -> {final double KS = 0.065;} //Kansas
-            case "KY" -> {final double KY = 0.06;} //Kentucky
-            case "LA" -> {final double LA = 0.0445;} //Louisiana
-            case "ME" -> {final double ME = 0.055;} //Maine
-            case "MD" -> {final double MD = 0.06;} //Maryland
-            case "MA" -> {final double MA = 0.0625;} //Massachusetts
-            case "MI" -> {final double MI = 0.06;} //Michigan
-            case "MN" -> {final double MN = 0.0688;} //Minnesota
-            case "MS" -> {final double MS = 0.07;} //Mississippi
-            case "MO" -> {final double MO = 0.0423;} //Missouri
-            case "MT" -> {final double MT = 0.00;} //Montana
-            case "NE" -> {final double NE = 0.055;} //Nebraska
-            case "NV" -> {final double NV = 0.0685;} //Nevada
-            case "NH" -> {final double NH = 0.00;} //New Hampshire
-            case "NJ" -> {final double NJ = 0.0663;} //New Jersey
-            case "NM" -> {final double NM = 0.0513;} //New Mexico
-            case "NY" -> {final double NY = 0.04;} //New York
-            case "NC" -> {final double NC = 0.0475;} //North Carolina
-            case "ND" -> {final double ND = 0.05;} //North Dakota
-            case "OH" -> {final double OH = 0.0575;} //Ohio
-            case "OK" -> {final double OK = 0.045;} //Oklahoma
-            case "OR" -> {final double OR = 0.00;} //Oregon
-            case "PA" -> {final double PA = 0.06;} //Pennsylvania
-            case "RI" -> {final double RI = 0.07;} //Rhode Island
-            case "SC" -> {final double SC = 0.06;} //South Carolina
-            case "SD" -> {final double SD = 0.045;} // South Dakota
-            case "TN" -> {final double TN = 0.07;} //Tennessee
-            case "TX" -> {final double TX = 0.0625;} //Texas
-            case "UT" -> {final double UT = 0.061;} //Utah
-            case "VT" -> {final double VT = 0.06;} //Vermont
-            case "VA" -> {final double VA = 0.053;} //Virginia
-            case "WA" -> {final double WA = 0.065;} //Washington
-            case "WV" -> {final double WV = 0.06;} //West Virginia
-            case "WI" -> {final double WI = 0.05;} //Wisconsin
-            case "WY" -> {final double WY = 0.04;} //Wyoming
+            case "AL" -> {tax = 0.04; }//Alabama
+            case "AK" -> {tax = 0.00;} //Alaska
+            case "AZ" -> {tax = 0.056;} //Arizona
+            case "AR" -> {tax = 0.065;} //Arkansas
+            case "CA" -> {tax = 0.0725;} //California
+            case "CO" -> {tax = 0.029;} //Colorado
+            case "CT" -> {tax = 0.0635;} //Connecticut
+            case "DE" -> {tax = 0.00;} //Delaware
+            case "DC" -> {tax = 0.06;} //District of Columbia
+            case "FL" -> {tax = 0.06;} //Florida
+            case "GA" -> {tax = 0.04;} //Georgia
+            case "HI" -> {tax = 0.04;} //Hawaii
+            case "ID" -> {tax = 0.06;} //Idaho
+            case "IL" -> {tax = 0.0625;} //Illinois
+            case "IN" -> {tax = 0.07;} //Indiana
+            case "IA" -> {tax = 0.06;} //Iowa
+            case "KS" -> {tax = 0.065;} //Kansas
+            case "KY" -> {tax = 0.06;} //Kentucky
+            case "LA" -> {tax = 0.0445;} //Louisiana
+            case "ME" -> {tax = 0.055;} //Maine
+            case "MD" -> {tax = 0.06;} //Maryland
+            case "MA" -> {tax = 0.0625;} //Massachusetts
+            case "MI" -> {tax = 0.06;} //Michigan
+            case "MN" -> {tax= 0.0688;} //Minnesota
+            case "MS" -> {tax = 0.07;} //Mississippi
+            case "MO" -> {tax = 0.0423;} //Missouri
+            case "MT" -> {tax = 0.00;} //Montana
+            case "NE" -> {tax = 0.055;} //Nebraska
+            case "NV" -> {tax = 0.0685;} //Nevada
+            case "NH" -> {tax = 0.00;} //New Hampshire
+            case "NJ" -> {tax = 0.0663;} //New Jersey
+            case "NM" -> {tax = 0.0513;} //New Mexico
+            case "NY" -> {tax = 0.04;} //New York
+            case "NC" -> {tax = 0.0475;} //North Carolina
+            case "ND" -> {tax = 0.05;} //North Dakota
+            case "OH" -> {tax = 0.0575;} //Ohio
+            case "OK" -> {tax = 0.045;} //Oklahoma
+            case "OR" -> {tax = 0.00;} //Oregon
+            case "PA" -> {tax = 0.06;} //Pennsylvania
+            case "RI" -> {tax = 0.07;} //Rhode Island
+            case "SC" -> {tax = 0.06;} //South Carolina
+            case "SD" -> {tax = 0.045;} // South Dakota
+            case "TN" -> {tax = 0.07;} //Tennessee
+            case "TX" -> {tax = 0.0625;} //Texas
+            case "UT" -> {tax = 0.061;} //Utah
+            case "VT" -> {tax = 0.06;} //Vermont
+            case "VA" -> {tax = 0.053;} //Virginia
+            case "WA" -> {tax = 0.065;} //Washington
+            case "WV" -> {tax= 0.06;} //West Virginia
+            case "WI" -> {tax = 0.05;} //Wisconsin
+            case "WY" -> {tax = 0.04;} //Wyoming
 
 
         }
+        return tax;
     }
 }
 
